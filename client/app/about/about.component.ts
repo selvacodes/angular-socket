296388b404a462dda7fdbeb10439e951d904bef2
Selva Ganapathy <selva.g@listertechnologies.com>
2018-02-22 22:46:21 +0530

    working socket

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import * as io from "socket.io-client";
+// import io from "socket.io-client";
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html'
+  selector: "app-about",
+  templateUrl: "./about.component.html"
 })
-export class AboutComponent {
-
-  constructor() { }
-
+export class AboutComponent implements OnInit {
+  private url = "http://localhost:3000";
+  private socket;
+  @Input() progress = "10";
+  constructor() {}
+  ngOnInit(): void {
+    this.socket = io(this.url);
+    console.log(io, this.socket.connect(), this.socket);
+    this.progress = "20";
+    this.socket.on("progress", _ => {
+      const newProgress = parseInt(this.progress, 10) + 10;
+      this.progress = newProgress.toString();
+    });
+  }
 }
--- server/app.ts
+++ server/app.ts
@@ -1,50 +1,64 @@
-import * as bodyParser from 'body-parser';
-import * as dotenv from 'dotenv';
-import * as express from 'express';
-import * as morgan from 'morgan';
-import * as mongoose from 'mongoose';
-import * as path from 'path';
+import * as bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import * as express from "express";
+import * as morgan from "morgan";
+import * as mongoose from "mongoose";
+import * as path from "path";
 
-import setRoutes from './routes';
+import setRoutes from "./routes";
 
 const app = express();
-dotenv.load({ path: '.env' });
-app.set('port', (process.env.PORT || 3000));
+dotenv.load({ path: ".env" });
 
-app.use('/', express.static(path.join(__dirname, '../public')));
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.set("port", process.env.PORT || 3000);
+
+app.use("/", express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let mongodbURI;
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   mongodbURI = process.env.MONGODB_TEST_URI;
 } else {
   mongodbURI = process.env.MONGODB_URI;
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
 
 mongoose.Promise = global.Promise;
-const mongodb = mongoose.connect(mongodbURI);
+//const mongodb = mongoose.connect(mongodbURI);
 
-mongodb
-  .then((db) => {
-    console.log('Connected to MongoDB');
+//mongodb
+//.then(db => {
+//console.log("Connected to MongoDB");
 
-    setRoutes(app);
+//setRoutes(app);
 
-    app.get('/*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
+//app.get("/*", function(req, res) {
+//res.sendFile(path.join(__dirname, "../public/index.html"));
+//});
 
-    if (!module.parent) {
-      app.listen(app.get('port'), () => {
-        console.log('Angular Full Stack listening on port ' + app.get('port'));
-      });
-    }
+//if (!module.parent) {
+//app.listen(app.get("port"), () => {
+//console.log("Angular Full Stack listening on port " + app.get("port"));
+//});
+//}
+//})
+//.catch(err => {
+//console.error(err);
+//
+//});
+io.set("origins", "http://localhost:4200");
 
-  })
-  .catch((err) => {
-    console.error(err);
+console.log("IO");
+io.on("connection", socket => {
+  console.log("user connected");
+
+  setInterval(() => {
+    io.emit("progress", { type: "new-message", text: "10" });
+  }, 5000);
 });
-
+http.listen(3000);
 export { app };
